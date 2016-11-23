db26d3cadc79f4865655c2f5c98f4953e2a65356
Emily Zhai <ezhai24@gmail.com>
2016-11-23 02:57:32 -0800

    testing new img paths

--- data/projects.js
+++ data/projects.js
@@ -2,7 +2,7 @@ var PROJECTS = [
     {
         "name": "Design Narrative",
         "description": "This storyboard is the product of an assingment I did junior year at UW for my Design Thinking class. The requirements were to \"identify a project or assignment that you have created during your time at UW and storyboard your process using constructed, found, and/or modified image\".",
-        "tileImage": "../img/info360storyboard-tile.jpg",
-        "contentImage": "../img/info360storyboard-content.jpg"
+        "tileImage": "../info360-portfolio/img/info360storyboard-tile.jpg",
+        "contentImage": "../info360-portfolio/img/info360storyboard-content.jpg"
     }
 ]
