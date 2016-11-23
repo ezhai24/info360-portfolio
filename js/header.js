6386d8d7c0ebf092707dfe09c540f8afbf3b4c2b
Emily Zhai <ezhai24@gmail.com>
2016-11-23 02:59:28 -0800

    fixed link/img paths

--- js/header.js
+++ js/header.js
@@ -13,13 +13,13 @@ class Header extends React.Component {
                 <h1 id="pageTitle">Emily Zhai</h1>
                 <ul id="menu">
                     <li className="menuItem">
-                        <a href="../index.html">Projects</a>
+                        <a href="../info360-portfolio/index.html">Projects</a>
                     </li>
                     <li className="menuItem">
-                        <a href="../about.html">About</a>
+                        <a href="../info360-portfolio/about.html">About</a>
                     </li>
                     <li className="menuItem">
-                        <a href="../contact.html">Contact</a>
+                        <a href="../info360-portfolio/contact.html">Contact</a>
                     </li>
                 </ul>
             </div>
